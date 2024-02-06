a95be3452a0dde2e0a73a864afe543617f82f7a9
Shail Desai <shaidesa@iu.edu>
2024-02-06 19:55:16 +0000

    Implement v2: Read file contents when loaded to GCS

--- index.js
+++ index.js
@@ -2,37 +2,20 @@ const {Storage} = require('@google-cloud/storage');
 const csv = require('csv-parser');
 
 exports.readObservation = (file, context) => {
-    // console.log(`  Event: ${context.eventId}`);
-    // console.log(`  Event Type: ${context.eventType}`);
-    // console.log(`  Bucket: ${file.bucket}`);
-    // console.log(`  File: ${file.name}`);
+    console.log(`Reading file: ${file.name}`);
 
     const gcs = new Storage();
-
     const dataFile = gcs.bucket(file.bucket).file(file.name);
 
     dataFile.createReadStream()
-    .on('error', () => {
-        // Handle an error
-        console.error(error);
+    .on('error', (error) => {
+        console.error('Error reading file:', error);
     })
     .pipe(csv())
     .on('data', (row) => {
-        // Log row data
-        // console.log(row);
-        printDict(row);
+        console.log('Row data:', row);
     })
     .on('end', () => {
-        // Handle end of CSV
-        console.log('End!');
-    })
-}
-
-// HELPER FUNCTIONS
-
-function printDict(row) {
-    for (let key in row) {
-        console.log(key + ' : ' + row[key]);
-        console.log(`${key} : ${row[key]}`);
-    }
-}
+        console.log('End of CSV file');
+    });
+};
